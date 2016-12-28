e0b713f3e26b39ecd968b73daf49f34414e056fb
czarnebloto <grabinski.bar@gmail.com>
2016-12-28 17:11:58 +0100

    Create klasy.h

---
+++ klasy.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifndef klasy_h
+#define klasy_h
+using namespace std;
+
+//KLASY
+
+class wojownik
+{
+public:
+	int sila;
+	int obrona;
+	int zwinnosc;
+	int magia;
+	int zdr;
+
+};
+
+class mag
+{
+public:
+	int sila;
+	int obrona;
+	int zwinnosc;
+	int magia;
+	int zdr;
+};
+
+class zlodziej
+{
+public:
+	int sila;
+	int obrona;
+	int zwinnosc;
+	int magia;
+	int zdr;
+
+};
+class ork
+{
+public:
+	int sila;
+	int obrona;
+	int zwinnosc;
+	int magia;
+	int zdr;
+};
