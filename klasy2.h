85d3125b68d5b7a0328e91a7fe5b1ca82c79d10b
czarnebloto <grabinski.bar@gmail.com>
2017-01-04 19:05:22 +0100

    Create klasy2.h

---
+++ klasy2.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifndef klasy_h
+#define klasy_h
+using namespace std;
+
+//KLASY
+
+
+class player
+{
+public:
+	char klasa;
+	int sila;
+	int obrona;
+	int zwinnosc;
+	int magia;
+	int zdr;
+	int zloto;
+	int lek;
+};
+class enemy
+{
+public:
+	char klasa;
+	int sila;
+	int obrona;
+	int zwinnosc;
+	int magia;
+	int zdr;
+	
+
+};
+
+
+#endif
